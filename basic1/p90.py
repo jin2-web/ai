28a1195e33cc69894beca07f1ef6d1261c209000
jin2-web <jhunhee0075@gmail.com>
2023-12-13 17:55:55 +0900

    2장 연습문제 3장 if문

---
+++ basic1/p90.py
@@ -0,0 +1,21 @@
+# 온라인 서점의 책 결재 금액
+price = 20000
+discount = 10
+delivery = 3000
+
+# 만약에 배송료가 2000이상이면 비싸다. 아니면 저렴하다
+if delivery >= 2000 :
+    print("비싸다")
+    delivery = delivery * ( 1- 0.02 )
+else :
+    print("저렴하다")  
+
+pay = price - (price * (discount/100)) + delivery
+print(f"책값은 {price}, 할인율은 {discount}%")
+print(f"배송료 {delivery}, 결제금액은 { int(pay) }")
+  
+
+# 만약에 배송료가 2000이상이면 
+#    비싸다 출력하고
+#    배송료 * 0.02 추가할인해서 결제금액 다시 계산
+# 아니면 저렴하다
---
+++ basic1/p92.py
@@ -0,0 +1,37 @@
+# 92페이지 연습문제 2장 1~3번 풀어보기
+# 95 심화문제
+# s2-1 환산 프로그램
+"""
+kg = input("변환할 킬로그램(kg)은")
+print("-"*30)
+print("킬로그램     파운드    온스 ")
+print("-"*30)
+pound = int(kg) * 2.204623
+ounce = int(kg) * 35.273962
+# print(f"{kg}    {pound}   {ounce}")
+print("%d       %.2f      %.2f"%(int(kg), pound, ounce ))
+print("-"*30)
+
+
+# s2-2 
+phone = input("휴대번호는?") 
+# 방법1) "-" 찾아서 "" 바꾸기
+deleteHy = phone.replace("-", "")
+print(f"입력된 휴대번호 : {phone}")
+print(f"하이픈 삭제된 휴대번호 {deleteHy}")
+# 방법2) split("-")  010-1234-5678
+deletHy2 = phone.split("-") 
+print( deletHy2[0], deletHy2[1], deletHy2[2], sep="" )
+"""
+
+# 2-13 
+number = "37366366845"
+print( number[ -2: : ])
+
+# 2-12
+string = "가는 말이 고와야 오는 말이 곱다"
+print( string[ 6 : 14])
+
+
+
+
