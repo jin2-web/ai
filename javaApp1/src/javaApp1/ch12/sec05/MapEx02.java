3413190e3f97d595191c1890b8edf2aa64a19009
jin2-web <jhunhee0075@gmail.com>
2023-12-26 14:13:46 +0900

    Map, TreeSet, comparable 비교하기

---
+++ javaApp1/src/javaApp1/ch12/sec05/MapEx02.java
@@ -0,0 +1,93 @@
+package javaApp1.ch12.sec05;
+
+import java.nio.file.spi.FileSystemProvider;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MapEx02 {
+
+	public static void main(String[] args) {
+		// Map 
+           Member m1 = new Member( "a100", 123);
+           Member m2 = new Member( "a200", 234);
+           Member m3 = new Member( "a300", 345);
+		// Map 키는 1 값은 m1, 
+           HashMap<Integer, Member> hmap =new HashMap< Integer, Member  >();
+        // 데이터 입력하기
+           hmap.put( 1, m1);
+           hmap.put( 2, m2);
+           hmap.put( 3, m3);
+        // "a300", 345 출력하기  힌트 : 키는 3이다
+           System.out.println(  hmap.get( 3 ));
+        // 키들만 출력하기  1 2 3   
+           Set<Integer> keys =hmap.keySet();
+           Iterator<Integer> it = keys.iterator();
+           while( it.hasNext() ) {
+        	   Integer itKey= it.next() ;
+        	   System.out.println( itKey + " " + hmap.get( itKey ));
+           }
+           
+         // Map예제 키 - id 값 - pwd
+//           Member m1 = new Member( "a100", 123);
+//           Member m2 = new Member( "a200", 234);
+//           Member m3 = new Member( "a300", 345);
+             HashMap<Member, Integer> hMap2 = new HashMap< Member, Integer>();
+             hMap2.put( m1, m1.pwd);
+             hMap2.put( m2, m2.pwd);
+             hMap2.put( m1, m1.pwd);
+           // a100일때 pwd는?
+             System.out.println(  hMap2.get( m1 )  );
+             
+             Member m4 = new Member( "a300", 345);
+             hMap2.put( m1, m1.pwd);
+             
+             Member m5 = new Member( "a300", 345);
+             hMap2.put( m5, m5.pwd);
+             
+             System.out.println(   hMap2.size() );
+           //동등객체를 id와 pwd 같으면 같은 것 코딩하려면 
+             Set<Member> keys1 = hMap2.keySet();
+             Iterator<Member> k1 = keys1.iterator();
+             while( k1.hasNext() ) {
+            	 System.out.println( k1.next() );
+             }
+             
+	}
+
+}
+
+class Member {
+	
+	String id;
+	Integer pwd;
+	
+	public Member(String id, Integer pwd) {
+		this.id = id;
+		this.pwd = pwd;
+	}
+	
+	@Override
+	public String toString() {
+		return id + "  " + pwd ;
+	}
+	
+	@Override
+	public int hashCode() {
+		//동등객체 판단은 hashCode()가 같고 equals()가 같으면 동등객체임
+		return id.hashCode() + pwd.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member m =(Member) obj;
+			if( id.equals(m.id) && pwd==m.pwd )  return true;
+		}
+		return false;
+	}
+	
+	
+	
+	
+}
---
+++ javaApp1/src/javaApp1/ch12/sec05/PropertiesEx.java
@@ -0,0 +1,18 @@
+package javaApp1.ch12.sec05;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesEx {
+
+	public static void main(String[] args) throws IOException {
+		// 663
+		Properties properties = new Properties();
+		
+		properties.load(PropertiesEx.class.getResourceAsStream("database.properties"));
+
+		System.out.println( properties.getProperty("driver") ); 
+		System.out.println( properties.getProperty("url") ); 
+	}
+
+}
