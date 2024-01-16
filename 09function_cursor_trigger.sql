a88dce9be12d046de3a3483a2a40f214ce6cc162
jin2-web <jhunhee0075@gmail.com>
2024-01-16 14:48:41 +0900

    지도데이터

--- 09function_cursor_trigger.sql
+++ 09function_cursor_trigger.sql
@@ -382,12 +382,166 @@ select * from usertbl ;
 update usertbl set birthYear = 1977 where userId='BBK' ;
 update usertbl set addr = '서울' where userId='EJW' ;
 
+update usertbl set addr = '경기' where userId='JKW' ;
 select * from backup_userTbl ;
 
-
+-- 467
+-- 삭제가 발생했을 때 
+delimiter //
+ create trigger  bud  -- 언제 누구에게 
+    after  delete 
+    on usertbl 
+    for each row 
+ begin
+     insert into backup_userTbl 
+     values (
+               OLD.userID, OLD.name, OLD.birthYear, old.addr, 
+              old.mobile1, old.mobile2, old.height, old.mDate,
+              '삭제', curDate(), current_user()
+            );
+ end // 
+delimiter ; 
  
- 
+select * from usertbl ;
+delete from usertbl  where userid = 'YJS' ; 
 
+select * from backup_userTbl ;
 
+-- 469쪽
+-- 삽입후에 경고 오류 발생시키고 경고 메세지 띄우기
+delimiter //
+ create trigger  uti -- 언제 누구에게 
+    after  insert
+    on usertbl 
+    for each row 
+ begin
+    signal sqlstate '45000' set message_text ='데이터의 입력을 시도했습니다. 귀하의 정보가 서버에 기록되었습니다.';
+ end // 
+delimiter ; 
+select * from usertbl;
+insert into usertbl values('ABC','에비씨', 1977, '서울','011','1111111',181,'2019-12-25','잠재고객' ); 
 
+select * from usertbl;
+
+drop trigger uti; 
+
+-- before 트리거
+
+-- old 변경되기 전에 자료, new 변경되고 난 후 자료 
+-- 요구사항 - 입력할 때 생일 잘못 입력되지 않도록 1900이전 입력이면 0 입력 또는 올해 년도보다 이후의 년도 입력 되었어 0 입력 
+delimiter //
+create trigger ubi 
+  before insert
+  on usertbl
+  for each row
+ begin
+   if new.birthyear < 1900 then
+      set new.birthYear = 0;
+   elseif new.birthYear > year( curdate() ) then   
+      set new.birthYear = year( curdate() ) ;
+   end if ;   
+ end //
+delimiter ;
+
+insert into usertbl values('DDD','디디디', 1877, '서울','011','1111111',181,'2019-12-25','잠재고객' );
+insert into usertbl values('EEE','이이이', 2877, '서울','011','1111111',181,'2019-12-25','잠재고객' );  
+select * from usertbl ;
+
+-- 주소가 '평양' 입력되면 '외국인' 
+-- 전화번호가 '999' 입력이되면 '010' 바꿔서 입력하기 
+drop trigger ubi2;
+delimiter //
+create trigger ubi2 
+  before insert
+  on usertbl
+  for each row
+ begin
+   if new.addr = '평양' then
+      set new.addr = '외국';
+   elseif new.mobile1 = '999' then   
+      set new.mobile1 = '010' ;
+   end if ;   
+ end //
+delimiter ;
+
+insert into usertbl values('FFF','에프에', 1877, '평양','011','1111111',181,'2019-12-25','잠재고객' );
+insert into usertbl values('GGG','지지지', 2877, '서울','999','1111111',181,'2019-12-25','잠재고객' );  
+select * from usertbl ;
+
+-- 생성한 트리거들을 확인하기 
+show triggers from sqldb ;
+
+-- 다중 트리거
+-- 구매 1  물품테이블 -1  배송테이블 +1
+
+drop database if exists triggerDb;
+create database if not exists triggerDb;
+use triggerDb;
+
+-- 구매테이블
+create table orderTbl
+  ( orderNo int auto_increment  primary key, 
+    userId varchar(5),
+    prodName varchar(5),
+    orderamount int ) ;
+-- 물품테이블
+create table prodTbl
+  ( prodName varchar(5) ,
+    account int ) ; 
+-- 배송테이블
+create table deliverTbl 
+  ( deliverNo  int auto_increment primary key,
+    prodName varchar(5),
+    account int ); 
+-- 물품테이블에 물건을 삽입하기 
+insert into prodTbl values ( '사과', 100 ); 
+insert into prodTbl values ( '배', 100 );
+insert into prodTbl values ( '귤', 100 );
+
+select * from prodTbl ; 
+    
+-- 트리거 
+-- 요구사항 구매테이블 삽입되면 물품테이블에서 갯수만큼 차감해서 업데이트 한다 
+delimiter //
+create trigger orderTg 
+   after insert
+   on orderTbl 
+   for each row 
+begin
+   update prodTbl set account = account - new.orderamount where prodName = new.prodName ;
+end //
+delimiter ;
+
+-- 요구사항 물품테이블을 업데이트 된 후에 배송테이블에 삽입하기  5개 
+delimiter //
+create trigger prodTg
+  after update 
+  on prodTbl
+  for each row 
+begin
+  declare orderAmount int; -- 변수 선언
+  set orderAmount = OLD.account - New.account ; -- 100 - 95 = 5
+  insert into deliverTbl values ( null, new.prodName, orderAmount ) ;
+end //
+delimiter ; 
+
+--   
+select * from ordertbl;
+desc ordertbl;
+insert into ordertbl values( null, 'BBK', '사과', 5 ) ;
+
+show triggers ;
+
+select * from ordertbl;
+select * from prodTbl;
+select * from deliverTbl ;
+
+insert into ordertbl values( null, 'KBS', '배', 10 ) ; 
+
+select * from ordertbl;
+select * from prodTbl;
+select * from deliverTbl ;
+
+   
+    
 
