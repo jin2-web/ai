25437dd7bfef98b015e551f1a5615aa6936f10bf
jin2-web <jhunhee0075@gmail.com>
2024-03-11 17:48:03 +0900

    6장 컴퓨터비젼

--- 3/연습문제9.py
+++ 3/연습문제9.py
@@ -12,14 +12,23 @@ def draw( event, x, y, flags, param) :
     cv.imshow( 'Drawing', img )    
 
 img = cv.imread('rose.png') # jpg 채널3개 png 채널 4개
-patch = img[250:350, 170:270, : ]
 
-img = cv.rectangle(img, (170, 250), (270, 350), (255, 0,0), 3) 
+cv.namedWindow('Drawing') # 윈도우창 이름 - 마우스 이벤트 창이름
+cv.imshow( 'Drawing', img )
+
+cv.setMouseCallback('Drawing', draw)
+
+while(True) :
+    if cv.waitKey(1) == ord('q'):
+        break
+
+patch = img[ y1:y2, x1:x2, :   ] 
+img = cv.rectangle(img, (x1,y1), (x2, y2), (255,0,0) , 2)
 patch1 = cv.resize( patch, dsize=(0,0), fx=5, fy=5, interpolation=cv.INTER_NEAREST)
 patch2 = cv.resize( patch, dsize=(0,0), fx=5, fy=5, interpolation=cv.INTER_LINEAR)
 patch3 = cv.resize( patch, dsize=(0,0), fx=5, fy=5, interpolation=cv.INTER_CUBIC)
 
-cv.imshow( 'Original', img )
+cv.imshow('Original', img)
 cv.imshow('Resize nearest', patch1 )
 cv.imshow( 'Resize bilinear', patch2)
 cv.imshow( 'Resize bicubic', patch3 )
